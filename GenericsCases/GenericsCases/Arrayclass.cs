76ca792ddd9890a02e14efdc5f710ab7e644767c
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-26 21:17:23 +0530

    [Harini]: Write a program to print the same

---
+++ GenericsCases/GenericsCases/Arrayclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsCases
+{
+    internal class Arrayclass
+    {
+        public  void toprint(int[] inputArry)
+        {
+            foreach (int element in inputArry)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+        public  void toprint(double[] inputArry)
+        {
+            foreach (double element in inputArry)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+        public void toprint(char[] inputArry)
+        {
+            foreach (char element in inputArry)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------");
+        }
+    }
+}
---
+++ GenericsCases/GenericsCases/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace generic
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            GenericsCases.Arrayclass program = new GenericsCases.Arrayclass();  
+            int[] intArry = { 1, 2, 3, 4, 5, 6 };
+            double[] doubleArry = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+            char[] charArry = { 'H', 'A', 'R', 'I', 'N', 'I' };
+
+            program.toprint(intArry);
+            program.toprint(doubleArry);
+            program.toprint(charArry);
+
+        }
+    }
+  }
